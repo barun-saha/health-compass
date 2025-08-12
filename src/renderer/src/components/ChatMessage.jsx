7c366924c2d75843048f0f9238ef71a62d6a3eff
Barun Saha <barun.saha04@gmail.com>
2025-08-12 21:29:01 +0530

    style: Use theme to apply msg bg color

--- src/renderer/src/components/ChatMessage.jsx
+++ src/renderer/src/components/ChatMessage.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import { Box, Paper, IconButton, Tooltip } from '@mui/material'
+import { Box, Paper, IconButton, Tooltip, useTheme } from '@mui/material'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 const ChatMessage = ({ msg }) => {
   const [tooltipText, setTooltipText] = useState('Copy message')
+  const theme = useTheme()
 
   // Function to copy text to clipboard
   const handleCopy = async () => {
@@ -37,7 +38,10 @@ const ChatMessage = ({ msg }) => {
           padding: '2px 8px',
           maxWidth: '80%',
           marginBottom: '20px',
-          backgroundColor: msg.role === 'user' ? 'chat.user' : 'chat.assistant',
+          backgroundColor:
+            msg.role === 'user'
+              ? theme.palette.chat.user[theme.palette.mode]
+              : theme.palette.chat.assistant[theme.palette.mode],
           position: 'relative' // Needed for absolute positioning of copy icon
         }}
       >
